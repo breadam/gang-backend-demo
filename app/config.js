3300eca17828f5b6a0791ef5b812d3b4f1a1f206
breadam <bilgi@breadam.com>
2015-01-10 21:46:53 +0200

    Signed-off-by: breadam <bilgi@breadam.com>

--- app/config.js
+++ app/config.js
@@ -3,7 +3,9 @@ module.exports = (function(){
 	var environment = process.env.NODE_ENV || 'development';
 	var config = {
 	};
-
+	
+	console.log(environment);
+	
 	if(environment =='development'){
 			
 		config.port = 8000;
